16134aced584c45cfa572b9a0883d3af07be1b3e
Philippe Thierry <phil@reseau-libre.net>
2026-03-08 11:11:51 +0100

    doc: adding missing doxygen info on platform driver API definition

--- include/merlin/platform/driver.h
+++ include/merlin/platform/driver.h
@@ -37,17 +37,20 @@ struct platform_fops {
 	merlin_platform_isr_fn_t	  isr;
 };
 
-/*
- * generic to all platform devices FOPS definition
+/**
+ * @brief generic to all platform devices FOPS definition
+ *
  * This structure aim to be used by various platform device families (i2c, SPI, usart, and so on) as
  * a part of the device declaration
+ *
  */
-
 struct platform_device_driver {
 	/**< unique device handle associated to the device, forge at boot time */
 	devh_t 	  devh;
+	/**< device label as set in the dts file using sentry,label attribute */
 	uint32_t  label;
-	const devinfo_t *devinfo; /**< device information retrieved from DTS, based on the label */
+	/**< device information retrieved from DTS, based on the label */
+	const devinfo_t *devinfo;
 	/**< device name for debug purpose */
 	const char * name;
 	/**< device compatible field declared in the bus driver, used at probe time */
@@ -56,14 +59,26 @@ struct platform_device_driver {
 	void * driver_fops;
 	/**< generic platform operations, common to all platform drivers */
 	struct platform_fops platform_fops;
+	/**< device type, that allows to discriminates the way merlin interact with the dts backend */
 	device_type_t type;
 };
 
 /* platform level utility functions, that do not need driver-level implementation */
 
-/* probe for the existence and ownership of the given device handle */
+
 /**
- * Note: because self type varies depending on the driver family, the probe function is
+ * @brief probe for the existence and ownership of the given device handle
+ *
+ * This function registers the device driver and validate that the corresponding device
+ * do exist in the device tree, is owned by the task and update the driver metadata with
+ * usefull informations the driver can use afterward.
+ *
+ * @param driver driver infos to be registered and updated by merlin with backend informations
+ * @param label device label as set in the dts using sentry,label attribute
+ *
+ * @return STATUS_OK if device found and metadata updated, or other status codes depending on the error
+ *
+ * @note because self type varies depending on the driver family, the probe function is
  * defined as a generic one that only take the device label as argument, and return the
  * device handle if the device is found and owned by the current task, or 0 otherwise.
  * For each device type, this prototype definition override the void* using the corresponding
@@ -71,17 +86,48 @@ struct platform_device_driver {
  */
 Status merlin_platform_driver_register(struct platform_device_driver *driver, uint32_t label);
 
-/* Map given device handle into the task memory */
+/**
+ * @brief Map given device handle into the task memory
+ *
+ * This function help with device memory mapping using Senty uapi.
+ * This function can be used only after merlin_platform_driver_register was successfully executed
+ *
+ * @param self device driver metadata
+ *
+ */
 Status merlin_platform_driver_map(struct platform_device_driver *self);
 
-/* Unmap given device handle from the task memory */
+/**
+ * @brief Unmap given device handle from the task memory
+ *
+ * This function help with device memory mapping using Senty uapi.
+ * This function can be used only after merlin_platform_driver_register was successfully executed
+ * The function can be called when the device is mapped. Otherwise it will return an error.
+ *
+ * @param self device driver metadata
+ *
+ */
 Status merlin_platform_driver_unmap(struct platform_device_driver *self);
 
-/* resolve driver context in the registered driver(s) for given IRQn using DTS backend,
- * and call the fops->isr routine for it
+/**
+ * @brief this function aim to automatically call the IRQ handler of the various registered drivers
+ *
+ * This function aim to be used in the wait_for_event() Sentry UAPI when EVENT_INTERRUPT is received.
+ * As Merlin knows all device drivers and associated device IRQ identifier, it can properly call the
+ * corresponding driver interrupt service routine based on the previously registered drivers metadata.
+ *
+ * This function allows a task to manipulate multiple device drivers for multiple devices without the
+ * need for knowing each device interrupt identifier.
  */
 Status merlin_platform_driver_irq_displatch(uint32_t IRQn);
 
+/**
+ * @brief configure the driver's target device associted GPIO, when there are some
+ *
+ * This function use the dts-declared pinmux informations in order to configure the GPIO controller
+ * using the Sentry kernel UAPI.
+ * Once successfully called, the GPIOs are properly set so that the device can interract with outer world.
+ */
 Status merlin_platform_driver_configure_gpio(struct platform_device_driver *self);
 
 #endif/*!MERLIN_PLATFORM_H*/
